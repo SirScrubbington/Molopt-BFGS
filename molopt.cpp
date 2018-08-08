e1c2a7b4869e4477039fdef5093dcfa8fc48217c
Scrubbs <damonmurdoch1@gmail.com>
2018-08-08 18:38:23 +1000

    first commit

---
+++ molopt.cpp
@@ -0,0 +1,19 @@
+// Filename: molopt.cpp
+// Author: Scrubbs
+// Date: 2018-8-7
+// Description: molopt implementation file
+
+#ifndef I_MY3sy2M43uDRW3krlK4Oy3Et3o1B7
+#define I_MY3sy2M43uDRW3krlK4Oy3Et3o1B7
+
+#include "molopt.h"
+
+#include "bfgs/lbfgs.h"
+
+int main(int argc, char ** argv, char ** envp)
+{
+
+	return 0;
+}
+
+#endif // I_MY3sy2M43uDRW3krlK4Oy3Et3o1B7
---
+++ molopt.h
@@ -0,0 +1,10 @@
+// Filename: molopt.h
+// Author: Scrubbs
+// Date: 2018-8-7
+// Description: molopt.h header file
+
+#ifndef I_NfAv5xV18d4u1Tj758VJpX4i18fsL
+#define I_NfAv5xV18d4u1Tj758VJpX4i18fsL
+
+
+#endif // I_NfAv5xV18d4u1Tj758VJpX4i18fsL

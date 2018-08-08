32e63042b43f8b8f22a9070558c6c5b59eaea14b
Scrubbs <damonmurdoch1@gmail.com>
2018-08-08 15:39:06 +0100

    debugging

--- molopt.cpp
+++ molopt.cpp
@@ -8,11 +8,182 @@
 
 #include "molopt.h"
 
-#include "bfgs/lbfgs.h"
+double * getGradientList(double * gradients, double * real, Point * coords, Matrix<double> * distance, int n)
+{
+	for(int m=0;m<n;m++)
+	{
+		gradients[m]=0.0f;
+		for(int i=0;i<m;i++)
+		{
+			for(int j=m+1;j<n;j++)
+			{
+				gradients[m] += 
+				((1.0f / pow(distance->get(i,j),14)) - (1.0f / pow(distance->get(i,j),8))) * 
+				((coords[i].x-coords[j].x) * (coords[m].y - coords[i].y) + 
+				 (coords[i].y-coords[j].y) * (coords[j].x - coords[m].x));
+			}
+		}
+		gradients[m] *= -12.0f;
+	}
+	return gradients;
+}
+
+double * getRealAngleList(double * alphas, double * real, int n)
+{
+	real[0] = alphas[0];
+	
+	for(int i=1;i<n;i++)
+		real[i] = real[i-1] + alphas[i];
+	
+	return real;
+}
+
+Point getCoords(double distance, double angle)
+{
+	return {distance*cos(DEG2RAD*angle),distance*sin(DEG2RAD*angle)};
+}
+
+Point * getSystemCoordinates(double * real, Point * coords, int n)
+{
+	for(int i=2;i<n;i++)
+	{
+		coords[i] = coords[i-1] + getCoords(1.0f,real[i-1]);
+	}	
+	
+	return coords;
+}
+
+double getR(double x, double y)
+{
+	return sqrt(pow(x,2) + pow(y,2));
+}
+
+Matrix<double> * getEulerianDistanceMatrix(Point * coords, Matrix<double> * distance, int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			//i == j ? distance.set(i,j,0.0f) : distance.set(i,j,getR());
+			if (i==j)
+			{
+				distance->set(i,j,0.0f);
+			}
+			else
+			{
+				Point d = (coords[i] - coords[j]).absolute();
+				distance->set(i,j,getR(d.x,d.y));
+			}
+		}
+	}
+	return distance;
+}
+
+double getSystemEnergy(Point * coords, Matrix<double> * distance, int n)
+{
+	double V = 0;
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i;j<n;j++)
+		{
+			
+			if (i==j)
+				continue;
+			
+			//Point distance = (coords[i] - coords[j]).absolute();
+			
+			//double r = getR(distance.x,distance.y);
+			
+			V = V + ((1.0f/pow(distance->get(i,j),12.0f))-(2.0f/pow(distance->get(i,j),6.0f)));
+		}
+	}
+	
+	return V;
+	
+}
 
 int main(int argc, char ** argv, char ** envp)
 {
+	int n=0;
+	
+	if (argc > 1)
+		n = atoi(argv[1]);
+	else
+		printf("Usage: ./molopt[.exe/.out] natoms");
+	
+	if(!n)
+		return 0;
+	
+	Matrix<double> * distance = new Matrix<double>(n,n);
+	
+	double * alphas = (double*)malloc(sizeof(*alphas)*n);
+	
+	double * real = (double*)malloc(sizeof(*real)*n);
+	
+	double * gradients = (double*)malloc(sizeof(*gradients)*n); 
+	
+	Point * coords = (Point*)malloc(sizeof(*coords)*n);
+	
+	for(int i=0;i<n;i++)
+	{
+		alphas[i]=0.0f;
+		gradients[i]=0.0f;
+		coords[i]={0.0f,0.0f};
+	}
+	
+	coords[1]={1.0f,0.0f};
+	
+	//for(int i=0;i<n;i++)
+	//	printf("%f\n",alphas[i]);
+	//printf("\n");
+	
+	real = getRealAngleList(alphas,real,n);
+	
+	//for(int i=0;i<n;i++)
+	//	printf("%f\n",real[i]);
+	//printf("\n");
+	
+	coords = getSystemCoordinates(real,coords,n);
+	
+	//for(int i=0;i<n;i++)
+	//	printf("(%f %f)\n",coords[i].x,coords[i].y);
+	//printf("\n");
+	
+	distance = getEulerianDistanceMatrix(coords,distance,n);
+	
+	//for(int i=0;i<distance->y; i++)
+	//{
+	//	for(int j=0;j<distance->x;j++)
+	//	{
+	//		printf("%f,",distance->get(i,j));
+	//	}
+	//	printf("\n");
+	//}
+	
+	double E = getSystemEnergy(coords,distance,n);
+	
+	gradients = getGradientList(gradients,real,coords,distance,n);
+	
+	for(int i=0;i<n;i++)
+		printf("(%i , %f)\n",i,gradients[i]);
+	printf("\n");
+	
+	printf("%f\n",E);
+	
+	
+	{	
 
+			real = getRealAngleList(alphas,real,n);
+
+			coords = getSystemCoordinates(real,coords,n);
+
+			distance = getEulerianDistanceMatrix(coords,distance,n);
+			
+			double E = getSystemEnergy(coords,distance,n);
+			
+			gradients = getGradientList(gradients,real,coords,distance,n);
+	}
 	return 0;
 }
 
--- molopt.h
+++ molopt.h
@@ -6,5 +6,142 @@
 #ifndef I_NfAv5xV18d4u1Tj758VJpX4i18fsL
 #define I_NfAv5xV18d4u1Tj758VJpX4i18fsL
 
+#include "bfgs/lbfgs.c"
 
+#include <cfloat>
+#include <cstdio>
+#include <cstdlib>
+#include <stdexcept>
+
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+
+#define DEG2RAD M_PI/180
+#define RAD2DEG 180/M_PI
+
+#define NO_SOLUTION 99
+
+const double tolerance = pow(10,-6);
+
+const double optimal[] = {
+	-1.0f,
+	-3.0f,
+	-5.1f,
+	-7.2f,
+	-9.3f,
+	-12.5f,
+	-14.7f,
+	-16.9f,
+	-20.1f,
+	-22.3f,
+	-25.5f,
+	-27.8f,
+	-31.0f,
+	-33.2f,
+	-36.5f,
+	-38.7f,
+	-42.0f,
+	-45.3f,
+	-47.5f,
+	-50.8f,
+	-53.0f,
+	-56.3f
+	-59.6f,
+	-61.8f,
+	-65.1f,
+	-68.4f,
+	-70.0f,
+	-74.0f,
+	-77.2f,
+	-79.5f,
+	-82.8f,
+	-86.1f,
+	-88.3f,
+	-91.7f,
+	-95.0f,
+	-98.3f,
+	-100.5f,
+	-103.8f,
+	-107.1f,
+	-109.4f,
+	-112.7f,
+	-116.0f,
+	-119.3f,
+	-121.6f,
+	-124.9f,
+	-128.6f,
+	-131.5f,
+	-133.8f,
+	-137.1f,
+	-140.5f,
+	-143.7f,
+	-146.0f,
+	-149.3f,
+	-152.7f,
+	NO_SOLUTION,
+	NO_SOLUTION,
+	NO_SOLUTION,
+	NO_SOLUTION,
+	NO_SOLUTION,
+	-166.1f
+	};
+
+template <class T>
+class Matrix
+{
+public:
+	int x,y;
+	T * data;
+
+	Matrix(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+		data = (T*)malloc(sizeof(*data)*x*y);
+	}
+	
+	inline void set(int i, int j, double v)
+	{
+		if((i*x+j) > (x*y))
+			throw std::invalid_argument("Attempting to access out of bounds index");
+		
+		data[i*x+j]=v;
+	}
+	
+	inline T get(int i, int j)
+	{
+		if((i*x+j) > (x*y))
+			throw std::invalid_argument("Attempting to access out of bounds index");
+		
+		return data[i*x+j];
+	}
+};
+	
+struct Point 
+{
+	double x,y;
+	inline Point operator=(Point a)
+	{
+		x=a.x;
+		y=a.y;
+		return a;
+	}
+	inline Point operator+(Point a)
+	{
+		return {a.x+x,a.y+y};
+	}
+	inline Point operator-(Point a)
+	{
+		return {x-a.x,y-a.y};
+	}
+	inline bool operator==(Point a)
+	{
+		return (a.x==x && a.y==y);
+	}
+	inline Point absolute()
+	{
+		return {fabs(x),fabs(y)};
+	}
+};
 #endif // I_NfAv5xV18d4u1Tj758VJpX4i18fsL

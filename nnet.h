1480e7b069227b3086a519385e59567c876e6658
Scrubbs <damonmurdoch1@gmail.com>
2018-08-24 09:34:44 +1000

    made it more object orientated

--- nnet.h
+++ nnet.h
@@ -1,4 +1,4 @@
-// Filename: nnet.h
+// Filename: nnet.hidden
 // Author: Scrubbs
 // Date: 2018-8-20
 // Description: nnet.h header file
@@ -12,206 +12,247 @@
 
 #include "Matrix.h"
 
+/*
+double transfer(double activation)
+{
+	return 1.0f / (1.0f + exp(-activation));
+}
+
+double transferDerivative(double output)
+{
+	return output * (1.0f - output);
+}
+*/
+
+template<typename nntyp>
+class Neuron
+{
+public:
+	
+	nntyp output,bias,delta;
+
+	std::vector<nntyp> * weights;
+	
+	nntyp transfer(const std::vector<nntyp> & inputs)
+	{
+		return 1.0f / (1.0f + exp(-activate(inputs)));
+	}
+	
+	nntyp transferDerivative()
+	{
+		return output * (1.0f - output);
+	}
+	
+	nntyp activate(const std::vector<nntyp> & inputs)
+	{
+		nntyp activation = bias;
+		
+		for( int i = 0; i < weights.size(); i++)
+		{
+			activation += weights.at(i) * inputs.at(i);
+		}
+		
+		return activation;
+		
+	}
+	
+	Neuron(int nInputs)
+	{
+		std::mt19937 gen(time(0));	
+		std::uniform_real_distribution<double> dist(-1.0f,1.0f);
+		auto wgt = std::bind(dist,gen);
+		
+		weights = new std::vector<nntyp>(nInputs);
+		
+		for(int i = 0; i < nInputs; i++)
+		{
+			weights->at(i) = wgt();
+		}
+		
+		bias = wgt();
+	}
+	
+	~Neuron()
+	{
+		delete weights;
+	}
+};
+
+template<typename nntyp>
+class NetworkLayer
+{
+	public:
+	
+	std::vector<Neuron<nntyp> *> * neurons;
+	
+	NetworkLayer(int nNeurons)
+	{
+		neurons = new std::vector<Neuron<nntyp> *>(nNeurons);
+	}
+	
+	~NetworkLayer()
+	{
+		delete neurons;
+	}
+};
+
 template<typename nntyp>
 class NeuralNetwork
 {
 private:
 	
-	int nX, nY, nH;
+	std::vector<int> * shape;
 	
-	std::vector<nntyp> * x;
-	std::vector<nntyp> * h;
-	std::vector<nntyp> * y;
+	std::vector<NetworkLayer<nntyp>*> * layers;
 	
-	std::vector<nntyp> * xDelta;
-	std::vector<nntyp> * hDelta;
-	std::vector<nntyp> * yDelta;
-	
-	Matrix<nntyp> * w1;
-	Matrix<nntyp> * w2;
-		
 public:
-
-	double transfer(double activation)
+	
+	std::vector<nntyp> * forwardPropogate(std::vector<nntyp> & row)
 	{
-		return 1.0f / (1.0f + exp(-activation));
-	}
-
-	void forwardPropogate()
-	{
-		double activation, output;
 		
-		// input -> hidden
+		nntyp activation;
 		
-		std::vector<nntyp> newH();
+		std::vector<nntyp> * input = new std::vector<nntyp>(row);
 		
-		for(int i = 0; i < nH; i++)
+		for(int i = 0; i < layers->size(); i++)
 		{
 			
-			activation = h[i];
+			std::vector<nntyp> newInputs();
 			
-			// hidden layer activation
-			for(int j = 0; j < nX; j++)
+			for(int j = 0; j < layers->at(i)->size(); j++)
 			{
-				activation += w1->get(i,j) * x.at(j);
+				newInputs.push_back(layers->at(i)->at(j)->transfer());
 			}
 			
-			// transfer function
-			output = transfer(activation);
+			delete input;
 			
-			//newH.push_back(output);
-			h.at(i) = output;
+			input = new std::vector<nntyp>(newInputs);
+			
 		}
 		
-		// hidden -> output
+		return input;
 		
-		std::vector<nntyp> newY();
+	}
+	
+	void backPropogate(const std::vector<nntyp> & expected)
+	{
 		
-		for(int i = 0; i < nY; i++)
+		nntyp error;
+		
+		for(int i = layers->size(); i < 0; i--)
 		{
-			activation = y[i];
+			std::vector<nntyp> errors();
 			
-			// output layer activation
-			for(int j = 0; j < nH; j++)
+			if (i < layers->size()-1)
 			{
-				activation += w2->get(i,j) * h.at(j);
+				for(int j = 0; j < layers->at(i)->size(); j++)
+				{
+					error = 0.0f;
+					
+					for(int k = 0; k < layers->at(i + 1)->size(); k++)
+					{
+						auto neuron = layers->at(i + 1)->at(k);
+						error += neuron->weights.at(j) * neuron->delta;
+						
+					}
+					
+					errors.push_back(error);
+					
+				}
 			}
 			
-			// transfer function
-			output = transfer(activation);
-			
-			//newY.push_back(output);
-			y.at(i) = output;
-		}
-	}
-	
-	double transferDerivative(double output)
-	{
-		return output * (1.0f - output);
-	}
-	
-	void backpropY(std::vector<nntyp> * expected)
-	{
-		std::vector<nntyp> errors();
-		
-		// calculate errors
-		for(int i = 0; i < nY; i++)
-		{
-			errors.push_back(expected.at(i) - y.at(i));
-		}
-		
-		// calculate deltas
-		for(int i = 0; i < nY; i++)
-		{
-			yDelta.at(i) = errors.at(i) * transferDerivative(y.at(i));
-		}
-	}
-	
-	void backpropH(std::vector<nntyp> * expected)
-	{
-		std::vector<nntyp> errors();
-		
-		double error;
-		
-		// calculate errors
-		for(int i = 0; i < nH; i++)
-		{
-			error = 0.0f;
-		
-			for( int j = 0; j < nY; j++)
+			else
 			{
-				error += w2->get(i,j) * yDelta.at(j);
+				for(int j = 0; j < layers->at(i)->size(); j++)
+				{
+					auto neuron = layers->at(i)->at(j);
+					
+					
+					
+				}
 			}
 			
-			errors.append(error);
-		}
-		
-		// calculate deltas
-		for(int i = 0; i < nH; i++)
-		{
-			hDelta.at(i) = errors.at(i) * transferDerivative(h.at(i));
+			for(int j = 0; j < layers->at(i)->size(); j++)
+			{
+				auto neuron = layers->at(i)->at(j);
+				neuron->delta = errors.at(j) * transferDerivative(neuron->output);
+			}
 		}
 	}
 	
-	void backpropX(std::vector<nntyp> * expected)
+	void updateWeights(const std::vector<nntyp> & row, double lRate)
 	{
-		std::vector<nntyp> errors();
 		
-		double error;
+		std::vector<nntyp> * inputs;
 		
-		// calculate errors
-		for(int i = 0; i < nX; i++)
+		for(int i = 0; i < layers->size(); i++)
 		{
-			for(int j = 0; j < nH; j++)
+			
+			
+			if(i > 0)
 			{
-				error += w1->get(i,j) * hDelta.at(j);
+				inputs = new std::vector<nntyp>(layers->at(i)->size());
+				
+				for(int j = 0; j < layers->at(i)->size(); j++)
+				{
+					inputs->at(i) = layers->at(i)->at(j)->output;
+				}
+			}
+			else
+			{
+				inputs = new std::vector<nntyp>(row);
 			}
 			
-			errors.append(error);
+			for(int j = 0; j < layers->at(i)->size(); j++)
+			{
+				auto neuron = layers->at(i)->at(j);
+				
+				for(int k = 0; k < inputs->size(); k++)
+				{
+					neuron->weights(k) += lRate * neuron->delta * inputs.at(k);
+				}
+				
+				neuron->bias += lRate * neuron->delta;
+				
+			}
 			
-		}
-		
-		// calculate deltas
-		for(int i = 0; i < nX; i++)
-		{
-			xDelta.at(i) = errors.at(i) * transferDerivative(x.at(i));
+			delete inputs;
 		}
 	}
 	
-	void backPropogate(std::vector<nntyp> * expected)
+	void trainNetwork(std::vector<std::vector<nntyp>> * train, double lRate, int nEpoch, int nOutputs)
 	{
 		
-	}
-	
-	
-	NeuralNetwork(int nX, int nY, int nH)
-	{
+		double sumError;
 		
-		std::mt19937 gen(time(0));	
-		std::uniform_real_distribution<double> weights(-1.0f,1.0f);
-		
-		auto wgt = std::bind(weights,gen);
-		
-		this->nX = nX;
-		this->nY = nY;
-		this->nH = nH;
-		
-		this->x = new std::vector<nntyp>(nX);
-		this->h = new std::vector<nntyp>(nH);
-		this->y = new std::vector<nntyp>(nY);
-		
-		this->xDelta = new std::vector<nntyp>(nX);
-		this->hDelta = new std::vector<nntyp>(nH);
-		this->yDelta = new std::vector<nntyp>(nY);
-		
-		this->w1 = new Matrix<nntyp>(nX,nH);
-		this->w2 = new Matrix<nntyp>(nH,nY);
-		
-		for(int i = 0; i < nX; i++)
+		for(int epoch = 0; epoch < nEpoch; epoch++)
 		{
-			for(int j = 0; j < nH; j++)
+			sumError = 0.0f;
+			
+			for(int row = 0; row < train->size(); i++)
 			{
-				w1->set(i,j,wgt());
+				std::vector<nntyp> * output = forwardPropogate(train->at(row));
+				std::vector<nntyp> expected(nOutputs);
+				
 			}
 		}
+	}
+	
+	NeuralNetwork(const std::vector<int> & shape)
+	
+	{
+		this->shape = new std::vector<int>(shape);
+		this->layers = new std::vector<NetworkLayer<nntyp>*>(this->shape->size());
 		
-		for(int i = 0; i < nH; i++)
+		for(int i = 0; i < this->shape->size(); i++)
 		{
-			for(int j = 0; j < nY; j++)
-			{
-				w2->set(i,j,wgt());
-			}
+			layers->at(i) = new NetworkLayer<nntyp>(this->shape->at(i));
 		}
 	}
 	
 	~NeuralNetwork()
 	{
-		delete x;
-		delete y;
-		delete h;
-		
-		delete w1;
-		delete w2;
+		delete this->shape;
 	}
 };
 
